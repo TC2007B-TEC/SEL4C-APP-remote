78af898446d063d084f321eab7767e101f86aaaf
Raul V <74110279+Raul-VR@users.noreply.github.com>
2023-09-03 20:58:12 -0600

    Initial Commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# SEL4C-APP-remote
+This app is being developed for SEL4C and the IFE
---
+++ SEL4C-APP/ViewControllers/HomeVC.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SEL4C-APP
+//
+//  Created by Raúl Vélez on 03/09/23.
+//
+
+import UIKit
+
+class HomeVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
